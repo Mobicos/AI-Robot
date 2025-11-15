ce451f33eaef451a6c4530abf2fcd4f2cde31cbf
ZhaoYinqi <1770837893@qq.com>
2025-11-15 11:39:51 +0800

    feat(controller): 添加流式对话接口并忽略生产环境配置文件- 在 DeepSeekChatController 中新增 generateStream 方法支持流式输出 - 引入 Spring AI 相关类处理 Prompt 和消息流 - 配置 .gitignore 忽略生产环境的配置文件及密钥文件

--- src/main/java/com/q/airobotbackend/controller/DeepSeekChatController.java
+++ src/main/java/com/q/airobotbackend/controller/DeepSeekChatController.java
@@ -1,12 +1,14 @@
 package com.q.airobotbackend.controller;
 
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.deepseek.DeepSeekChatModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+import reactor.core.publisher.Flux;
 
 
 /**
@@ -19,6 +21,8 @@ public class DeepSeekChatController {
 
     @Autowired
     private DeepSeekChatModel chatModel;
+
+
     /**
      * 获取深度搜索结果
      *
@@ -30,4 +34,17 @@ public class DeepSeekChatController {
         return chatModel.call(message);
     }
 
+    /**
+     * 流式对话
+     * @param message
+     * @return
+     */
+    @GetMapping(value = "/generateStream", produces = "text/html;charset=utf-8")
+    public Flux<String> generateStream(@RequestParam(value = "message", defaultValue = "你是谁呢? 给我最详细的描述") String  message) {
+        // 构建提示词
+        Prompt prompt = new Prompt(new UserMessage( message));
+        // 流式输出
+        return chatModel.stream(prompt)
+                .mapNotNull(chatResponse -> chatResponse.getResult().getOutput().getText());
+    }
 }
